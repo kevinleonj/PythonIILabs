8fa2ef520cd5752b2a0d7142cbbcae52d0a35719
kevinleonj <kevinleonj@users.noreply.github.com>
2026-03-13 23:29:07 +0100

    Lab 6 stations

---
+++ src/app/routers/stations.py
@@ -0,0 +1,31 @@
+from typing import Any, Annotated
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.database import get_db
+from src.models import User, Station
+from src.app.security import get_current_user
+from src.app.schemas.stations import StationCreate, StationResponse
+
+
+router = APIRouter()
+
+
+@router.post("/", response_model=StationResponse, status_code=201)
+async def create_station(
+    station_data: StationCreate,
+    current_user: Annotated[User, Depends(get_current_user)],
+    db: Annotated[AsyncSession, Depends(get_db)],
+) -> Any:
+    if current_user.role != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Only admins can create stations",
+        )
+
+    new_station = Station(**station_data.model_dump())
+    db.add(new_station)
+    await db.commit()
+    await db.refresh(new_station)
+    return new_station
